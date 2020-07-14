1b76c4ddc3fb5975e0d4440cba23bb3f5a59abe6
BlokMireles <blokgraff@hotmail.com>
2020-07-14 18:02:27 -0500

    Creacion de primeras tablas
    Creacion de primeras tablas Tipo personal y personal

---
+++ ScriptBD/ScriptBDTablas.sql
@@ -0,0 +1,19 @@
+create DATABASE Checador;
+
+use checador;
+
+create TABLE TipoPersonal(
+IDTIPOPERSONAL INT AUTO_INCREMENT PRIMARY KEY not null,
+TIPOPERSONAL Varchar(50)
+);
+
+create TABLE Personal(
+RFC Varchar(50) AUTO_INCREMENT PRIMARY KEY not null,
+TIPOPERSONAL Varchar(50),
+NOMBRE  Varchar(50), 
+APELLIDOP Varchar(50), 
+APELLIDOM Varchar(50),
+FECHADENACIMIENTO  Varchar(50),
+FKTIPOPERSONAL int,
+foreign key (FKTIPOPERSONAL) REFERENCES TipoPersonal(IDTIPOPERSONAL)
+);
